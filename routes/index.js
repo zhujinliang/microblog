09f88ea09dd258b17361429ef8f9890f8445afa6
Jinliang Zhu <jzhu@redhat.com>
2013-06-21 04:33:59 +0800

    Modify the structure of routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,44 @@
 
-/*
- * GET home page.
- */
+exports.init = function(app) {
+  var user = require('./user');
+  var users = {
+    'jlzhu': {
+      name: 'Jinliang Zhu',
+      age: 23,
+      website: 'http://zhujinliang.github.io'
+    },
+  };
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+  app.get('/', function index(req, res) {
+    res.render('index', { title: 'Express' });
+  });
+
+  app.get('/users', user.list);
+
+  app.get('/hello', function hello(req, res) {
+    res.send('The time is '+ new Date().toString());
+  });
+
+  app.all('/user/:username', function(req, res, next) {
+    // Check if the user exists.
+    console.log('all method captured.');
+    if (users[req.params.username]) {
+      next();
+    } else {
+      next(new Error(req.params.username + ' does not exists.'));
+    }
+  });
+
+  app.get('/user/:username', function(req, res) {
+    // User exists, display the username.
+    res.send(JSON.stringify(users[req.params.username]));
+  });
+
+  app.get('/list', function list(req, res) {
+    res.render('list', {
+      title: 'List', 
+      items: [1991, 'jlzhu', 'express', 'Node.js']
+    });
+  });
+
+}
